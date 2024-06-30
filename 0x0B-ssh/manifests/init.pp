64a0d531b48f42715fe6d01341ce9df3210138f5
slwanmo009 <slwanmohammed9@gmail.com>
2024-06-30 14:02:46 -0700

    ly

---
+++ 0x0B-ssh/manifests/init.pp
@@ -0,0 +1,11 @@
+# This Puppet manifest configures the SSH client to use a specific private key and refuse password authentication
+
+class ssh_config {
+  file { '/home/ubuntu/.ssh/config':
+    ensure  => file,
+    owner   => 'ubuntu',
+    group   => 'ubuntu',
+    mode    => '0600',
+    content => template('ssh_config/config.erb'),
+  }
+}
